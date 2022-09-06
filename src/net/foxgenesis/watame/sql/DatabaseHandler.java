2347f12586e10a3b422918a9cbab5a815da1d9ad
Ashley <ashley@foxgenesis.net>
2022-09-05 22:57:01 -0500

    Create DatabaseHandler.java

---
+++ src/net/foxgenesis/watame/sql/DatabaseHandler.java
@@ -0,0 +1,41 @@
+package net.foxgenesis.watame.sql;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public interface DatabaseHandler {
+
+	/**
+	 * Check if the database is connected and is ready
+	 * for operations.
+	 * @return Returns {@code true} if is connected and ready
+	 * @see #registerStatement(String, String)
+	 * @see #hasStatement(String)
+	 */
+	public boolean isValid();
+	
+	/**
+	 * Compile a new {@link PreparedStatement} linked with {@code id} in
+	 * the database.
+	 * @param id - statement id
+	 * @param statement - SQL code to prepare
+	 * @return Compiled {@link PreparedStatement} that is ready to be used
+	 * @throws UnsupportedOperationException Thrown if id is already registered
+	 * or database connection is not valid
+	 * @throws SQLException
+	 * @see #hasStatement(String)
+	 * @see #isValid()
+	 */
+	public PreparedStatement registerStatement(String id, String statement) throws SQLException;
+	
+	/**
+	 * Returns if the database already has a {@link PreparedStatement}k
+	 * with the id {@code id}.
+	 * @param id - {@link PreparedStatement} id to check
+	 * @return Returns {@code true} if the database has already
+	 * registered this id.
+	 * @see #registerStatement(String, String)
+	 * @see #isValid()
+	 */
+	public boolean hasStatement(String id);
+}
