398b715d22142a8201b5d5ab881063f036099d28
Ashley <ashley@foxgenesis.net>
2022-10-02 21:08:04 -0500

    Update Constants.java

--- src/net/foxgenesis/watame/Constants.java
+++ src/net/foxgenesis/watame/Constants.java
@@ -2,9 +2,17 @@ package net.foxgenesis.watame;
 
 import java.net.URL;
 
+import javax.annotation.Nonnull;
+
 public final class Constants {
-	public static final URL DATABASE_SETUP_FILE = Constants.class.getResource("/main/resources/CreateDatabase.sql");
-	
-	public static final String[] VALID_DISCORD_DOMAINS = { "discordapp.com", "discordapp.net", "discord.com", "discord.new",
-			"discord.gift", "discord.gifts", "discord.media", "discord.gg", "discord.co", "discord.app", "dis.gd" };
+	public static final String[] VALID_DISCORD_DOMAINS = { "discordapp.com", "discordapp.net", "discord.com",
+			"discord.new", "discord.gift", "discord.gifts", "discord.media", "discord.gg", "discord.co", "discord.app",
+			"dis.gd" };
+
+	public static final URL DATABASE_SETUP_FILE = resource("CreateDatabase.sql");
+	public static final URL DATABASE_OPERATIONS_FILE = resource("sql statements.kvp");
+
+	private static final URL resource(@Nonnull String path) {
+		return Constants.class.getResource("/main/resources/" + path);
+	}
 }
