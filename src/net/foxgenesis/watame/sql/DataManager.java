65cd43a342775fea5f5895d152d7224a2afa55f2
Ashley <ashley@foxgenesis.net>
2022-10-11 20:50:35 -0500

    removed initial data retrieval

--- src/net/foxgenesis/watame/sql/DataManager.java
+++ src/net/foxgenesis/watame/sql/DataManager.java
@@ -189,7 +189,7 @@ public class DataManager implements IDatabaseManager, AutoCloseable {
 		Objects.nonNull(jda);
 
 		// Get all guilds from database
-		getAllGuildData();
+		//getAllGuildData();
 
 		// Insert all missing guilds to database
 //		logger.debug("Inserting missing guilds into database");
