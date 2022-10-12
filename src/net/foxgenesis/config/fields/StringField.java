64281d45bf582bf635ef372e9f394861b2f9cd86
Ashley <ashley@foxgenesis.net>
2022-10-11 21:41:06 -0500

    Create StringField.java

---
+++ src/net/foxgenesis/config/fields/StringField.java
@@ -0,0 +1,27 @@
+package net.foxgenesis.config.fields;
+
+import java.util.function.Function;
+
+import net.dv8tion.jda.api.entities.Guild;
+
+public class StringField extends ConfigField<String> {
+
+	public StringField(String name, Function<Guild, String> defaultValue, boolean isEditable) {
+		super(name, defaultValue, isEditable);
+	}
+
+	@Override
+	String optFrom(JSONObjectAdv config, Guild guild) {
+		return isPresent(config) ? config.optString(getName()) : getDefaultValue(guild);
+	}
+
+	@Override
+	String from(JSONObjectAdv config) {
+		return config.getString(getName());
+	}
+
+	@Override
+	void set(JSONObjectAdv config, String newState) {
+		config.put(getName(), newState);
+	}
+}
