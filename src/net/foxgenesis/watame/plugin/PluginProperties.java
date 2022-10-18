ab655f28cd510c24cd0ed9330b13a6f77ccc984a
Ashley <ashley@foxgenesis.net>
2022-10-18 18:45:18 -0500

    Update PluginProperties.java

--- src/net/foxgenesis/watame/plugin/PluginProperties.java
+++ src/net/foxgenesis/watame/plugin/PluginProperties.java
@@ -1,7 +1,25 @@
 package net.foxgenesis.watame.plugin;
 
 public @interface PluginProperties {
+	/**
+	 * Get the of the plugin
+	 * 
+	 * @return The name of this plugin
+	 */
 	public String name();
+
+	/**
+	 * Get the description of the plugin
+	 * 
+	 * @return The description of this plugin
+	 */
 	public String description();
+
+	/**
+	 * Get the version of this plugin. This is parsed by
+	 * {@link Runtime.Version#parse(String)}.
+	 * 
+	 * @return The version of this plugin.
+	 */
 	public String version();
 }
