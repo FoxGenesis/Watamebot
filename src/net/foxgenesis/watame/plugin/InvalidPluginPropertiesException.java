d244bdaa3bd801bb25fa3bd056b8bec3cc82e9e3
Ashley <ashley@foxgenesis.net>
2022-10-19 14:37:37 -0500

    removed unused class

--- src/net/foxgenesis/watame/plugin/InvalidPluginPropertiesException.java
+++
@@ -1,9 +0,0 @@
-package net.foxgenesis.watame.plugin;
-
-public class InvalidPluginPropertiesException extends UnsupportedOperationException {
-	private static final long serialVersionUID = 384299482971559711L;
-
-	public InvalidPluginPropertiesException() {}
-
-	public InvalidPluginPropertiesException(String message) { super(message); }
-}
