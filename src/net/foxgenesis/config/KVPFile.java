aa5bf42840e748d0a6301c82876db6d8354d9776
Ashley <ashley@foxgenesis.net>
2022-11-04 23:34:29 -0500

    added javadoc

--- src/net/foxgenesis/config/KVPFile.java
+++ src/net/foxgenesis/config/KVPFile.java
@@ -18,7 +18,7 @@ import net.foxgenesis.util.ResourceHelper;
 
 /**
  * Key Value Pair (KVP) file implementation.
- * 
+ *
  * @author Ashley
  *
  */
@@ -41,7 +41,7 @@ public class KVPFile {
 
 	/**
 	 * Parse a {@link File} into a KVP (Key Value Pair) file.
-	 * 
+	 *
 	 * @param file - {@link File} to parse
 	 * @throws MalformedURLException
 	 */
@@ -49,7 +49,7 @@ public class KVPFile {
 
 	/**
 	 * Parse a {@link URL} into a KVP (Key Value Pair) file.
-	 * 
+	 *
 	 * @param url - {@link URL} to parse
 	 */
 	public KVPFile(@Nonnull URL url) {
@@ -64,38 +64,81 @@ public class KVPFile {
 	 * by the implementing class, actions are performed inthe order of entry set
 	 * iteration (if an iteration order is specified.)Exceptions thrown by the
 	 * action are relayed to the caller.
-	 * 
+	 *
 	 * @param action - The action to be performed for each entry
 	 * @see Map#forEach(BiConsumer)
 	 */
 	public void forEach(BiConsumer<? super String, ? super String> action) { config.forEach(action); }
 
 	/**
-	 * NEED_JAVADOC
-	 * 
-	 * @param key
-	 * @return
-	 * @see HashMap#get(Object)
+	 * Returns the value to which the specified key is mapped, or {@code null} if
+	 * this map contains no mapping for the key.
+	 *
+	 * <p>
+	 * More formally, if this map contains a mapping from a key {@code k} to a value
+	 * {@code v} such that {@code Objects.equals(key, k)}, then this method returns
+	 * {@code v}; otherwise it returns {@code null}. (There can be at most one such
+	 * mapping.)
+	 *
+	 * <p>
+	 * If this map permits null values, then a return value of {@code null} does not
+	 * <i>necessarily</i> indicate that the map contains no mapping for the key;
+	 * it's also possible that the map explicitly maps the key to {@code null}. The
+	 * {@link #containsKey containsKey} operation may be used to distinguish these
+	 * two cases.
+	 *
+	 * @param key the key whose associated value is to be returned
+	 * @return the value to which the specified key is mapped, or {@code null} if
+	 *         this map contains no mapping for the key
+	 * @throws ClassCastException   if the key is of an inappropriate type for this
+	 *                              map (<a href=
+	 *                              "{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+	 * @throws NullPointerException if the specified key is null and this map does
+	 *                              not permit null keys (<a href=
+	 *                              "{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+	 * @see #getOrDefault(String, String)
 	 */
 	@Nullable
 	public String get(String key) { return config.get(key); }
 
 	/**
-	 * NEED_JAVADOC
-	 * 
-	 * @param key
-	 * @return
-	 * @see HashMap#getOrDefault(Object, Object)
+	 * Returns the value to which the specified key is mapped, or
+	 * {@code defaultValue} if this map contains no mapping for the key.
+	 *
+	 * @implSpec The default implementation makes no guarantees about
+	 *           synchronization or atomicity properties of this method. Any
+	 *           implementation providing atomicity guarantees must override this
+	 *           method and document its concurrency properties.
+	 *
+	 * @param key          the key whose associated value is to be returned
+	 * @param defaultValue the default mapping of the key
+	 * @return the value to which the specified key is mapped, or
+	 *         {@code defaultValue} if this map contains no mapping for the key
+	 * @throws ClassCastException   if the key is of an inappropriate type for this
+	 *                              map (<a href=
+	 *                              "{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+	 * @throws NullPointerException if the specified key is null and this map does
+	 *                              not permit null keys (<a href=
+	 *                              "{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+	 * @see #get(String)
 	 */
 	@Nullable
 	public String getOrDefault(String key, String defaultValue) { return config.getOrDefault(key, defaultValue); }
 
 	/**
-	 * NEED_JAVADOC
-	 * 
-	 * @param key
-	 * @return
-	 * @see HashMap#containsKey(Object)
+	 * Returns {@code true} if this map contains a mapping for the specified key.
+	 * More formally, returns {@code true} if and only if this map contains a
+	 * mapping for a key {@code k} such that {@code Objects.equals(key, k)}. (There
+	 * can be at most one such mapping.)
+	 *
+	 * @param key key whose presence in this map is to be tested
+	 * @return {@code true} if this map contains a mapping for the specified key
+	 * @throws ClassCastException   if the key is of an inappropriate type for this
+	 *                              map (<a href=
+	 *                              "{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+	 * @throws NullPointerException if the specified key is null and this map does
+	 *                              not permit null keys (<a href=
+	 *                              "{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
 	 */
 	public boolean containsKey(String key) { return config.containsKey(key); }
 
@@ -106,14 +149,14 @@ public class KVPFile {
 
 	/**
 	 * Checks if the configuration mapping is empty.
-	 * 
+	 *
 	 * @return Returns {@code true} if there are no configuration entries mapped
 	 */
 	public boolean isEmpty() { return config.isEmpty(); }
 
 	/**
 	 * Parse the resource {@link URL} into the configuration mapping.
-	 * 
+	 *
 	 * @throws IOException Thrown if an error occurs while reading the InputStream
 	 *                     of the resource
 	 */
