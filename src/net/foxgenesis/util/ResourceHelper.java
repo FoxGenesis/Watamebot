fe904a3e04d8876925ce726533e97eba5f6f9af5
Ashley <ashley@foxgenesis.net>
2022-11-04 20:05:12 -0500

    added method to load properties from url

--- src/net/foxgenesis/util/ResourceHelper.java
+++ src/net/foxgenesis/util/ResourceHelper.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +18,7 @@ public final class ResourceHelper {
 
 	/**
 	 * Read all lines from a resource
-	 * 
+	 *
 	 * @param path - {@link URL} path to the resource
 	 * @return Returns all lines as a {@link List<String>}
 	 * @throws IOException Thrown if an error occurs while reading the
@@ -43,5 +44,14 @@ public final class ResourceHelper {
 			// Return list
 			return list;
 		}
+	}
+
+	public static Properties getPropertiesResource(URL path) throws IOException {
+		logger.trace("Attempting to read resource: " + path);
+
+		Properties properties = new Properties();
+		properties.load(path.openStream());
+
+		return properties;
 	}
 }
