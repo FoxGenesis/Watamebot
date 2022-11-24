09fe4e5c53360653f40dfe1876d5ec5a5dea76bd
Ashley <ashley@foxgenesis.net>
2022-11-23 20:55:13 -0600

    Update AbstractDatabase.java

--- src/net/foxgenesis/watame/sql/AbstractDatabase.java
+++ src/net/foxgenesis/watame/sql/AbstractDatabase.java
@@ -4,15 +4,19 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
 
+import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,9 +26,7 @@ import net.foxgenesis.util.ResourceHelper;
 import net.foxgenesis.watame.ExitCode;
 
 /**
- * Abstract class used to create SQLite databases. Class is able to create
- * database file, setup tables and create {@link PreparedStatement
- * PreparedStatements} for all operations.
+ * NEED_JAVADOC
  *
  * @author Ashley
  *
@@ -34,168 +36,62 @@ public class AbstractDatabase implements AutoCloseable {
 	/**
 	 * Database logger
 	 */
+	@Nonnull
 	protected final Logger logger;
 
-	/**
-	 * Database folder used to store database files. Specifically the database file
-	 */
-	@Nonnull
-	private final File databaseFile;
-
-	@Nonnull
+	@Nullable
 	private final URL databaseSetupFile;
 
 	@Nonnull
 	private final URL databaseOperationsFile;
 
 	/**
-	 * SQL connection handler
+	 * On close methods
 	 */
-	private Connection connectionHandler;
+	@Nonnull
+	private final List<Runnable> onClose = new ArrayList<>();
+
+	/**
+	 * SQL connection pool
+	 */
+	@Nonnull
+	protected DataSource source;
 
 	/**
 	 * Map of registered statements. This is to ensure we do not make more of the
 	 * same prepared statements.
 	 */
-	private final ConcurrentHashMap<String, PreparedStatement> registeredStatements = new ConcurrentHashMap<>();
+	@Nonnull
+	private final ConcurrentHashMap<String, String> registeredStatements = new ConcurrentHashMap<>();
 
 	/**
-	 * Create a new instance using the default database folder of "./{@code repo}/".
-	 * <p>
-	 * This method is effectively equivalent to: <blockquote>
+	 * NEED_JAVADOC
 	 *
-	 * <pre>
-	 * new RoleStorageDatabase(new File("repo"))
-	 * </pre>
-	 *
-	 * </blockquote>
-	 *
-	 * @throws IllegalArgumentException if folder exists and is not a directory
-	 * @see #AbstractDatabase(File, URL, URL, String)
+	 * @param properties
 	 */
-	public AbstractDatabase(@Nonnull URL databaseSetupFile, @Nonnull URL databaseOperationsFile) {
-		this(new File("repo/database.db"), databaseSetupFile, databaseOperationsFile, "Database");
+	public AbstractDatabase(@Nonnull DatabaseProperties properties) {
+		Objects.requireNonNull(properties);
+
+		logger = LoggerFactory.getLogger(properties.name());
+
+		this.source = Objects.requireNonNull(properties.source());
+		this.databaseSetupFile = Objects.requireNonNull(properties.setupFile());
+		this.databaseOperationsFile = Objects.requireNonNull(properties.operationsFile());
 	}
 
 	/**
-	 * Create and initialize a new database at the specified file. Additionally,
-	 * creating prepared operations for use.
+	 * NEED_JAVADOC
 	 *
-	 * @param databaseFile           - {@link File} that will be used as the
-	 *                               repository
-	 * @param databaseSetupFile      - {@link URL} pointing to a file containing SQL
-	 *                               operations to initialize the new database
-	 * @param databaseOperationsFile - {@link URL} pointing to a KVP (Key Value
-	 *                               Pair) file containing SQL statements to create
-	 *                               {@link PreparedStatement PreparedStatements}
-	 *                               for
-	 * @param name                   - name to use for logging
-	 * @throws IllegalArgumentException if {@code folder} exists and is not a
-	 *                                  directory
-	 * @throws NullPointerException     if {@code folder} is null
-	 * @see #AbstractDatabase(URL, URL)
+	 * @throws SQLException
+	 * @throws UnsupportedOperationException
+	 * @throws IOException
 	 */
-	public AbstractDatabase(@Nonnull File databaseFile, @Nonnull URL databaseSetupFile,
-			@Nonnull URL databaseOperationsFile, @Nonnull String name) {
+	public synchronized void setup() throws SQLException, UnsupportedOperationException, IOException {
+		if (Objects.nonNull(databaseSetupFile))
+			setupDatabase(databaseSetupFile);
 
-		logger = LoggerFactory.getLogger("Role Storage");
-
-		this.databaseFile = Objects.requireNonNull(databaseFile);
-		this.databaseSetupFile = Objects.requireNonNull(databaseSetupFile);
-		this.databaseOperationsFile = Objects.requireNonNull(databaseOperationsFile);
-	}
-
-	/**
-	 * Attempt to connect to the database.
-	 *
-	 * @throws SQLException                  if a database access error occurs
-	 * @throws IOException                   thrown if an error occurs while reading
-	 *                                       the InputStream of the resource
-	 * @throws UnsupportedOperationException if connection to the database failed
-	 */
-	public void connect() throws SQLException, UnsupportedOperationException, IOException {
-		createDatabaseFile(databaseFile);
-
-		// Build connection path
-		String connectionPath = databaseFile.getPath();
-		logger.trace("Database path: {}", connectionPath);
-
-		// Attempt to connect to database file
-		logger.trace("Attempting connection to database");
-		connectionHandler = DriverManager.getConnection("jdbc:sqlite:" + connectionPath);
-
-		logger.info("Connected to SQL database");
-
-		setupDatabase(databaseSetupFile);
-		initalizeOperations(databaseOperationsFile);
-	}
-
-	/**
-	 * Check if the database connection is valid. This method checks if our
-	 * connection is not {@code null} and then asks the {@link Connection} if it's
-	 * valid with a one second timeout.
-	 *
-	 * @return If the database connection is valid and usable
-	 */
-	public boolean isConnectionValid() {
-		try {
-			// Check if our connection is valid with a one second timeout
-			return Objects.nonNull(connectionHandler) && connectionHandler.isValid(1000);
-		} catch (SQLException e) {
-			logger.error("Error while checking database connection", e);
-		}
-		return false;
-	}
-
-	/**
-	 * Compile a new {@link PreparedStatement} linked with {@code id} in the
-	 * database.
-	 *
-	 * @param id        - statement id
-	 * @param statement - SQL code to prepare
-	 * @return Compiled {@link PreparedStatement} that is ready to be used
-	 * @throws UnsupportedOperationException or database connection is not valid
-	 * @throws IllegalArgumentException      Thrown if id is already registered
-	 * @throws SQLException                  if a database access error occurs or
-	 *                                       this method is called on a closed
-	 *                                       connection
-	 * @see #getAndAssertStatement(String)
-	 */
-	private void registerStatement(String id, @Nonnull String statement) throws SQLException {
-		Objects.requireNonNull(statement);
-
-		// Check if id is already registered
-		if (registeredStatements.containsKey(id))
-			throw new IllegalArgumentException("Statement '" + id + "' already exists!");
-
-		// Check if connection is valid and ready
-		if (!isConnectionValid())
-			throw new UnsupportedOperationException("Not connected to database!");
-
-		// Create our statement
-		logger.trace("Creating PreparedStatement {} : {}", id, statement);
-		PreparedStatement preStatement = connectionHandler.prepareStatement(statement);
-
-		// Register our statement to ensure no duplicates are made
-		registeredStatements.put(id, preStatement);
-	}
-
-	/**
-	 * Get a {@link PreparedStatement} registered with key {@code id} and ensure it
-	 * exists. If the statement does not exist, this method will cause a program
-	 * exit with exit code {@link ExitCode.DATABASE_STATEMENT_MISSING}.
-	 *
-	 * @param id - key of the registered statement
-	 * @return the {@link PreparedStatement} if it is registered
-	 */
-	protected PreparedStatement getAndAssertStatement(@Nonnull String id) {
-		Objects.requireNonNull(id);
-
-		if (registeredStatements.containsKey(id))
-			return registeredStatements.get(id);
-
-		ExitCode.DATABASE_STATEMENT_MISSING.programExit(String.format("Statement with id '%s' is not registered!", id));
-		return null;
+		if (Objects.nonNull(databaseOperationsFile))
+			initalizeOperations(databaseOperationsFile);
 	}
 
 	/**
@@ -207,27 +103,27 @@ public class AbstractDatabase implements AutoCloseable {
 	 * @throws UnsupportedOperationException Thrown if not connected to database
 	 */
 	private void setupDatabase(@Nonnull URL url) throws IOException, UnsupportedOperationException {
-		// Check if we are connected to database
-		if (!isConnectionValid())
-			throw new UnsupportedOperationException("Not connected to database!");
+		Objects.requireNonNull(url);
 
 		// Read all lines in the file
 		logger.trace("Reading lines from SQL file");
 		List<String> lines = ResourceHelper.linesFromResource(url);
 
-		// Iterate on each line
-		for (String line : lines) {
-			if (line.startsWith("--"))
-				continue;
-			try (Statement statement = connectionHandler.createStatement()) {
+		try (Connection conn = source.getConnection()) {
+			// Iterate on each line
+			for (String line : lines) {
+				if (line.startsWith("--"))
+					continue;
+				try (Statement statement = conn.createStatement()) {
 
-				// Execute the current line
-				logger.trace("Executing SQL statement: " + line);
-				statement.execute(line);
-			} catch (SQLException e) {
-				ExitCode.DATABASE_SETUP_ERROR.programExit(e);
-				return;
+					// Execute the current line
+					logger.trace("Executing SQL statement: " + line);
+					statement.execute(line);
+				}
 			}
+		} catch (SQLException e) {
+			ExitCode.DATABASE_SETUP_ERROR.programExit(e);
+			return;
 		}
 	}
 
@@ -261,6 +157,108 @@ public class AbstractDatabase implements AutoCloseable {
 	}
 
 	/**
+	 * NEED_JAVADOC
+	 *
+	 * @param id
+	 * @param statement
+	 * @throws SQLException
+	 */
+	private void registerStatement(String id, @Nonnull String statement) throws SQLException {
+		Objects.requireNonNull(statement);
+
+		// Check if id is already registered
+		if (registeredStatements.containsKey(id))
+			throw new IllegalArgumentException("Statement '" + id + "' already exists!");
+
+		// Register our statement to ensure no duplicates are made
+		logger.trace("Creating PreparedStatement {} : {}", id, statement);
+		registeredStatements.put(id, statement);
+	}
+
+	/**
+	 * NEED_JAVADOC
+	 *
+	 * @param id
+	 * @return
+	 */
+	@CheckForNull
+	protected String getRawStatement(String id) { return registeredStatements.get(id); }
+
+	/**
+	 *
+	 * @param id
+	 * @return
+	 */
+	@Nullable
+	protected String assertRawStatement(String id) {
+		String statement = getRawStatement(id);
+		if (statement != null)
+			return statement;
+		ExitCode.DATABASE_STATEMENT_MISSING.programExit(String.format("Statement with id '%s' is not registered!", id));
+		return null;
+	}
+
+	/**
+	 * NEED_JAVADOC
+	 *
+	 * @param <R>
+	 * @param id
+	 * @param function
+	 * @return
+	 */
+	protected <R> R mapStatement(@Nonnull String id, @Nonnull StatementFunction<R> function) {
+		return mapStatement(id, function, null);
+	}
+
+	/**
+	 * NEED_JAVADOC
+	 *
+	 * @param <R>
+	 * @param id
+	 * @param function
+	 * @return
+	 */
+	protected <R> R mapStatement(String id, @Nonnull StatementFunction<R> function,
+			Consumer<SQLException> errorHandler) {
+		String raw = assertRawStatement(id);
+
+		try (Connection conn = source.getConnection(); PreparedStatement statement = conn.prepareStatement(raw)) {
+			return function.apply(statement);
+		} catch (SQLException e) {
+			if (errorHandler != null)
+				errorHandler.accept(e);
+			else
+				e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * NEED_JAVADOC
+	 *
+	 * @param id
+	 * @param consumer
+	 */
+	protected void callStatement(@Nonnull String id, @Nonnull StatementConsumer consumer) {
+		callStatement(id, consumer, null);
+	}
+
+	/**
+	 * NEED_JAVADOC
+	 *
+	 * @param id
+	 * @param consumer
+	 * @param errorHandler
+	 */
+	protected void callStatement(@Nonnull String id, @Nonnull StatementConsumer consumer,
+			Consumer<SQLException> errorHandler) {
+		mapStatement(id, statement -> {
+			consumer.accept(statement);
+			return null;
+		}, errorHandler);
+	}
+
+	/**
 	 * Create the database folder if it doesn't exist
 	 *
 	 * @param file - {@link File} used as the database folder
@@ -291,49 +289,36 @@ public class AbstractDatabase implements AutoCloseable {
 		}
 	}
 
-	@Override
-	public void close() throws SQLException {
-		// Check if we are connected
-		if (connectionHandler != null && !connectionHandler.isClosed()) {
-			// Close open statements
-			closeStatements();
-
-			// Close our connection
-			logger.info("Disconnecting from database");
-			connectionHandler.close();
-		} else {
-			logger.warn("Attempted to close already closed sql connection");
-		}
-	}
+	/**
+	 * NEED_JAVADOC
+	 *
+	 * @param onClose
+	 */
+	public void addCloseHandler(Runnable onClose) { this.onClose.add(onClose); }
 
 	/**
-	 * Close all open statements
+	 * NEED_JAVADOC
+	 *
+	 * @param toRemove
 	 */
-	private void closeStatements() {
-		// Close all statements
-		logger.trace("Closing open statements");
-		registeredStatements.entrySet().stream().forEach(e -> {
-			try {
-				// Close the statement
-				logger.trace("Closing statement " + e.getKey());
-				e.getValue().close();
-			} catch (SQLException e1) {
-				// Failed for some reason
-				logger.error("Failed to close statement '" + e.getKey() + "'!", e1);
-			}
-		});
-
-		// All open statements have been closed
-		logger.trace("All statements have been closed");
-
-		// Clear our map
-		registeredStatements.clear();
+	public void removeCloseHandler(Runnable toRemove) {
+		if (this.onClose.contains(toRemove))
+			this.onClose.remove(toRemove);
 	}
+
+	@Override
+	public synchronized void close() throws Exception { this.onClose.forEach(Runnable::run); }
 
 	@Override
 	public String toString() {
-		return "AbstractDatabase [databaseFile=" + databaseFile + ", databaseSetupFile=" + databaseSetupFile
-				+ ", databaseOperationsFile=" + databaseOperationsFile + ", connectionHandler=" + connectionHandler
-				+ ", registeredStatements=" + registeredStatements + "]";
+		return "AbstractDatabase [source=" + source + ", databaseSetupFile=" + databaseSetupFile
+				+ ", databaseOperationsFile=" + databaseOperationsFile + ", registeredStatements="
+				+ registeredStatements + "]";
 	}
+
+	@FunctionalInterface
+	protected interface StatementFunction<R> { public R apply(PreparedStatement statement) throws SQLException; }
+
+	@FunctionalInterface
+	protected interface StatementConsumer { public void accept(PreparedStatement statement) throws SQLException; }
 }
