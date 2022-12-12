b3cf3afe00943530ef12bcf9035a52bb6251c95d
Ashley <ashley@foxgenesis.net>
2022-12-11 20:00:03 -0600

    Update PluginProperties.java

--- src/net/foxgenesis/watame/plugin/PluginProperties.java
+++ src/net/foxgenesis/watame/plugin/PluginProperties.java
@@ -1,5 +1,12 @@
 package net.foxgenesis.watame.plugin;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PluginProperties {
 	/**
 	 * Get the of the plugin
