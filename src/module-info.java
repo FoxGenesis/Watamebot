0c1728a9f4021dc34c10803ba54a133394273b3c
Ashley <ashley@foxgenesis.net>
2023-07-27 17:39:02 -0500

    import clean up

--- src/module-info.java
+++ src/module-info.java
@@ -9,19 +9,18 @@
  */
 module watamebot {
 	requires transitive net.dv8tion.jda;
-	requires transitive org.json;
 	requires transitive org.slf4j;
 	requires transitive java.sql;
-	requires transitive com.zaxxer.hikari;
 	requires transitive org.apache.commons.configuration2;
 
 	requires static org.fusesource.jansi;
 	requires static org.jetbrains.annotations;
 
+	requires com.zaxxer.hikari;
 	requires org.apache.commons.lang3;
 	requires ch.qos.logback.core;
 	requires ch.qos.logback.classic;
-	requires java.desktop;
+	requires org.json;
 
 	exports net.foxgenesis.config;
 	exports net.foxgenesis.database;
