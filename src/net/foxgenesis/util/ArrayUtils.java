b343895d2dfab588b5f96a6d3a1c92a914ee0774
Ashley <ashley@foxgenesis.net>
2023-07-29 21:40:42 -0500

    added array utils

---
+++ src/net/foxgenesis/util/ArrayUtils.java
@@ -0,0 +1,21 @@
+package net.foxgenesis.util;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public final class ArrayUtils {
+
+	public static <T> String commaSeparated(T[] arr) {
+		return arr == null ? null : sep(Arrays.stream(arr));
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static String commaSeparated(int[] arr) {
+		return arr == null ? null : sep((Stream)Arrays.stream(arr));
+	}
+	
+	private static String sep(Stream<?> s) {
+		return s.map(a -> a + "").collect(Collectors.joining(","));
+	}
+}
