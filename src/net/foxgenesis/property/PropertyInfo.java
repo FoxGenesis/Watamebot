b0b22cdd09db7522c7c310d739b3006dbfe5ffa9
Ashley <ashley@foxgenesis.net>
2023-08-11 19:59:30 -0500

    Update PropertyInfo.java

--- src/net/foxgenesis/property/PropertyInfo.java
+++ src/net/foxgenesis/property/PropertyInfo.java
@@ -2,4 +2,14 @@ package net.foxgenesis.property;
 
 public record PropertyInfo(int id, String category, String name, boolean modifiable, PropertyType type) {
 
+	private static final String DISPLAY_FORMAT = "%d (%s) | [%s] %s";
+
+	public String getDisplayString() {
+		return DISPLAY_FORMAT.formatted(id, type, category, name);
+	}
+	
+	@Override
+	public String toString() {
+		return getDisplayString();
+	}
 }
