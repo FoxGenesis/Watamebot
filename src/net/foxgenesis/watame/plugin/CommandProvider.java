af43eab15282f0877b02bff9c9f3621027f33b01
Ashley <ashley@foxgenesis.net>
2023-08-20 19:34:43 -0500

    Update CommandProvider.java

--- src/net/foxgenesis/watame/plugin/CommandProvider.java
+++ src/net/foxgenesis/watame/plugin/CommandProvider.java
@@ -9,10 +9,6 @@ import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 public interface CommandProvider {
 	/**
 	 * Register all {@link CommandData} that this plugin provides.
-	 * <p>
-	 * <b>** {@code providesCommands} must be set to true in
-	 * {@code plugin.properties}! **</b>
-	 * </p>
 	 * 
 	 * @return Returns a non-null {@link Collection} of {@link CommandData} that
 	 *         this {@link Plugin} provides
